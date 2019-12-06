aa9ece5e193c6b65693c991a3be117c4ebb99234
mhamed sanaa <mhamed.sanaa6@gmail.com>
2019-12-06 16:53:36 +0100

    testing some changes

--- customer.class.php
+++ customer.class.php
@@ -35,23 +35,5 @@
                 echo $ex->getMessage();
              }
          }
-         public function updatestds()
-         {
-            $firstname=$_POST['firstname'];
-            $lastname=$_POST['lastname'];
-            $email=$_POST['email'];
-            $phone=$_POST['phone'];
-            $id=$_POST['id'];
-            
-            $req=$this->cnx->prepare('UPDATE students SET firstname=:firstname , lastname=:lastname , email=:email , phone=:phone WHERE id=:id');
-            
-            $req->bindParam(':firstname',$firstname);
-            $req->bindParam(':lastname',$lastname);
-            $req->bindParam(':email',$email);
-            $req->bindParam(':phone',$phone);
-            $req->bindParam(':id',$id);
-            $req->execute();
-            header('Location:index.php?alerte=edit');
-         }
     }
 ?>
