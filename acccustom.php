3c1a1bb49644ef94d93fe401827925c437f6b91d
mhamed sanaa <mhamed.sanaa6@gmail.com>
2019-12-07 17:38:42 +0100

    making some changes

--- acccustom.php
+++ acccustom.php
@@ -7,15 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=".\css\bootstrap.css">
     <script src="https://kit.fontawesome.com/7f1324c4ec.js" crossorigin="anonymous"></script>
     <script src=".\js\bootstrap.js"></script>
@@ -50,7 +41,5 @@
 
         </div>
     </div>
-</body>
-</html>
 </body>
 </html>
